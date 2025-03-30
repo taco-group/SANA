cbbe98dc94f351dd1de192fb666514c6d3217939
Junsong Chen <cjs1020440147@icloud.com>
2025-03-30 16:31:24 +0800

    recover inference ci test (#230)
    Signed-off-by: lawrence-cj <cjs1020440147@icloud.com>

--- tests/bash/entry.sh
+++ tests/bash/entry.sh
@@ -2,8 +2,8 @@
 set -e
 
 
-#echo "Testing inference"
-#bash tests/bash/test_inference.sh
+echo "Testing inference"
+bash tests/bash/test_inference.sh
 
 echo "Testing training"
 bash tests/bash/test_training_1epoch.sh
